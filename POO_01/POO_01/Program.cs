1d90fd8231696324687a2111234069aebbaa35bb
Jesus Miguel Marcelino Kranwinkel <jesus.marcelinok@gmail.com>
2020-02-27 16:59:10 -0400

    POO_01

---
+++ POO_01/POO_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_01
+{
+    class mostrarVehiculo
+    {
+        static void Main(string[] args)
+        {
+            /*Crear también una clase llamada mostrarVehiculo. Esta clase 
+            *deberá contener solo la función Main, que creara dos objetos de tipo 
+            *Vehículo, les asignará una marca y un modelo y les pedirá verVehiculo.*/
+
+            Vehiculo vehiculo1 = new Vehiculo();//objeto 1.
+            vehiculo1.SetVehiculo("Ford", "Explore");//Asignacion de marca y modelo.
+            vehiculo1.verVehiculo();//Mostrar valores.
+            Console.WriteLine("");
+            Vehiculo vehiculo2 = new Vehiculo();//Objeto 2.
+            vehiculo2.SetVehiculo("Mazda", "Demio");//Asignacion de marca y modelo.
+            vehiculo2.verVehiculo();//Mostrar valores.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ POO_01/POO_01/Vehiculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_01
+{
+    /*Crear una clase llamada Vehículo, esta debe tener los siguientes
+     *atributos: Marca y Modelo. También crear los métodos SetVehiculo, 
+     *de tipo void y con los parámetros string, que permita cambiar el 
+     *valor de la marca y del modelo. También debe crear un método verVehiculo, 
+     *que muestre en pantalla “El vehículo es: “, seguido de los datos recibidos 
+     *por el método.*/
+    class Vehiculo
+    {
+        //Agregamos los atributos de la clase.
+        public string marca;
+        public string modelo;
+        public Vehiculo()
+        {
+            //Los constructores de los atributos o de clase.
+            marca = "";
+            modelo = "";
+        }
+        public void SetVehiculo(string marca, string modelo)
+        {
+            //Metodo que permite cambiar los valores de los atributos.
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+        public void verVehiculo()
+        {
+            //Metodo que muestra en pantalla los valores de la clase.
+            Console.WriteLine("El vehiculo es: " + marca +" " + modelo);
+        }
+    }
+}
